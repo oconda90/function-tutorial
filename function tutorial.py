7c8abaddf3c37ca7da085430c3965595aed8f537
oconda90 <70242240+oconda90@users.noreply.github.com>
2020-09-07 13:15:09 -0700

    Add files via upload

---
+++ function tutorial.py
@@ -0,0 +1,26 @@
+def area(width, height):
+    result = width * height
+    return result
+result = area(5, 6)
+print(result)
+
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+result = subtract(8, 2)
+print(result)
+
+
+def divide(num1, num2):
+    result = num1 / num2
+    return result
+result = divide(21, 3)
+print (result)
+
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+result = add(9, 10)
+print(result)
